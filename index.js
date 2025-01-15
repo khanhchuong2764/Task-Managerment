9f83bfce1f7e9c1ed0950a669c8e56260b2b0d29
khanhchuong2764 <khanhchuong2764@gmail.com>
2025-01-15 07:58:47 +0700

    cai dat cors

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 require("dotenv").config();
 const bodyParser = require('body-parser');
 const database = require("./config/database");
+const cors = require('cors');
 const app = express();
 const port = process.env.Port;
 
 database.connect();
+
+// Tất cả tên miền được phép truy cập vào
+app.use(cors());
+// Cho phép 1 tên miền cụ thể được phép truy cập
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200
+// }
+// cors(corsOptions);
+
 
 // parse application/json
 app.use(bodyParser.json());
